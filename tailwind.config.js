9a3a08421c02a992e7c2d99292e6f02165c1898b
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-01-29 14:37:11 -0300

    feat: add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,18 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {}
+    extend: {},
+    colors: {
+      "green-1": "#081C15",
+      "green-2": "#1B4332",
+      "green-3": "#2D6A4F",
+      "green-4": "#40916C",
+      "green-5": "#52B788",
+      "green-6": "#74C69D",
+      "green-7": "#95D5B2",
+      "green-8": "#B7E4C7",
+      "green-9": "#D8F3DC"
+    }
   },
   plugins: []
 }
