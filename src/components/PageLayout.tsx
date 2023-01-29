f1f38df764fbef9ca4f1680e972b0e2edfda93dd
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-01-29 14:25:46 -0300

    feat: add PageLayout

---
+++ src/components/PageLayout.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head"
+import Header from "./Header"
+import logo from "../../public/logo.png"
+
+export default function PageLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Head>
+        <title>Pastesnip</title>
+        <meta name="description" content="A text storage website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href={logo.src} />
+      </Head>
+      <Header />
+      <main className="h-max ">{children}</main>
+    </>
+  )
+}
