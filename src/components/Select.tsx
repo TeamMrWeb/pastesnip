051074b2e02e8995ff8c42ec385d4b531aface02
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-01-31 17:51:19 -0300

    fix(Select): fix an issue with selected prop

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -7,7 +7,7 @@ export default function Select({
   name: string
   firstValue: string
   options: string[]
-  setValue: (value: string) => void
+  setValue?: (value: string) => void
 }) {
   return (
     <div className="w-full flex items-center justify-between text-gray-1 transition-all !duration-0">
@@ -16,11 +16,10 @@ export default function Select({
         id={name}
         name={name}
         className="select select-bordered w-full max-w-[200px] h-[40px] min-h-[20px] bg-green-1 !outline-none focus:border-green-4 transition-all focus:!duration-0"
-        onChange={e => setValue(e.target.value)}
+        onChange={e => setValue && setValue(e.target.value)}
+        defaultValue={firstValue}
       >
-        <option disabled selected>
-          {firstValue}
-        </option>
+        <option disabled>{firstValue}</option>
         {options?.map((option, index) => (
           <option key={index}>{option}</option>
         ))}
