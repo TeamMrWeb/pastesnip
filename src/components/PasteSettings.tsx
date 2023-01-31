f6423d97b1a08a6d4b950cc731697ff8d207100d
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-01-31 17:25:21 -0300

    refactor(PasteSettings): replace form logic

--- src/components/PasteSettings.tsx
+++ src/components/PasteSettings.tsx
@@ -1,21 +1,80 @@
+import { pasteSchema } from "@/utils/yupSchemas"
 import Button from "./Button"
 import Select from "./Select"
 import TextInput from "./TextInput"
+import { Form, Formik } from "formik"
+import * as Yup from "yup"
+import { useState } from "react"
+import FormInput from "./FormInput"
+import FormStringInput from "./FormStringInput"
+
+interface PasteSettingsProps {
+  title: string
+  syntaxHighlight: string
+  tags: string
+  exposure: string
+}
 
 export default function PasteSettings() {
+  const [exposure, setExposure] = useState("Public")
+  const [syntaxhighLight, setSyntaxhighLight] = useState("Javascript")
+
+  const handleSubmit = (values: PasteSettingsProps) => {
+    console.log(values)
+  }
+
   return (
     <aside className="pt-5 box-border">
       <h2 className="text-xl text-white border-b border-b-green-1 mb-4 pb-2">Paste Settings</h2>
-      <section className="flex flex-col gap-2">
-        <TextInput name="Title" placeholder="Title" />
-        <Select
-          name="Syntax Highlight"
-          firstValue="Select Syntax"
-          options={["Javascript", "HTML", "CSS"]}
-        />
-        <TextInput name="Tags" placeholder="" />
-        <Select name="Exposure" firstValue="Select your exposure" options={["Public", "Private"]} />
-        <Button text="Create new paste" type="submit" variant="primary" className="mt-5" />
+      <section>
+        <Formik
+          initialValues={{
+            title: "",
+            syntaxHighlight: "Javascript",
+            tags: "",
+            exposure: "Public"
+          }}
+          validationSchema={Yup.object(pasteSchema)}
+          onSubmit={values => handleSubmit(values)}
+        >
+          <Form className="flex flex-col gap-2">
+            <FormInput name="title">
+              <FormStringInput
+                type="text"
+                name="title"
+                label="Title"
+                placeholder="Title"
+                containerClassName="flex items-center justify-between text-gray-1"
+                fieldClassName="max-w-[200px]"
+              />
+            </FormInput>
+            <FormInput name="Syntax Highlight">
+              <Select
+                name="Syntax Highlight"
+                firstValue="Select Syntax"
+                options={["Javascript", "HTML", "CSS"]}
+                setValue={setExposure}
+              />
+            </FormInput>
+            <FormInput name="tags">
+              <FormStringInput
+                type="text"
+                name="Tags"
+                label="Tags"
+                placeholder=""
+                containerClassName="flex items-center justify-between text-gray-1"
+                fieldClassName="max-w-[200px]"
+              />
+            </FormInput>
+            <Select
+              name="Exposure"
+              firstValue="Select your exposure"
+              options={["Public", "Private"]}
+              setValue={setSyntaxhighLight}
+            />
+            <Button text="Create new paste" type="submit" variant="primary" className="mt-5" />
+          </Form>
+        </Formik>
       </section>
     </aside>
   )
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -10,12 +10,12 @@ export default function Select({
   setValue: (value: string) => void
 }) {
   return (
-    <div className="flex items-center justify-between text-gray-1">
+    <div className="w-full flex items-center justify-between text-gray-1 transition-all !duration-0">
       <label htmlFor="select">{name}:</label>
       <select
         id={name}
         name={name}
-        className="select select-bordered w-full max-w-[200px] h-[40px] min-h-[20px] bg-green-1 !outline-none"
+        className="select select-bordered w-full max-w-[200px] h-[40px] min-h-[20px] bg-green-1 !outline-none focus:border-green-4 transition-all focus:!duration-0"
         onChange={e => setValue(e.target.value)}
       >
         <option disabled selected>
