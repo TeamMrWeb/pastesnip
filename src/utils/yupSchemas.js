714cc75edbfe25df1ed6357ab19ab21a174ee657
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-01-31 17:51:03 -0300

    fix(pasteSchema): fix an issue with title regex

--- src/utils/yupSchemas.js
+++ src/utils/yupSchemas.js
@@ -34,7 +34,7 @@ export const signInSchema = {
 export const pasteSchema = {
   title: Yup.string()
     .max(20, "Must contain 20 characters or less")
-    .matches(/[^a-zA-Z]+/g, "Must contain only letters")
+    .matches(/^[a-zA-Z]*$/, "Must contain only letters")
     .required("Required"),
   syntaxHighlight: Yup.string().required("Required"),
   tags: Yup.string()
