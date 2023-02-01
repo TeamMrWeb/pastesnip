59ebba07b7bc3d3100e7de397a21af75d90b7864
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-02-01 18:15:09 -0300

    feat: add serverSideApolloFetching for dynamic ss data fetching

---
+++ src/apollo/serverSideApolloFetching.ts
@@ -0,0 +1,40 @@
+import { initializeApollo } from "@/apollo"
+import { DocumentNode } from "@apollo/client"
+import { getCookie } from "cookies-next"
+
+export const serverSideApolloFetching = async ({
+  fetch,
+  schema,
+  req,
+  res,
+  variables
+}: {
+  fetch: "query" | "mutation"
+  schema: DocumentNode
+  req: any
+  res: any
+  variables?: any
+}) => {
+  const accessToken = getCookie("accessToken", { req, res })
+  const apolloClient = initializeApollo()
+  const context = {
+    headers: {
+      auth: accessToken
+    }
+  }
+
+  if (fetch === "query") {
+    await apolloClient.query({
+      context,
+      query: schema
+    })
+  } else {
+    await apolloClient.mutate({
+      context,
+      mutation: schema,
+      variables
+    })
+  }
+
+  return apolloClient
+}
