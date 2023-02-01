2a9d6da16c1ba59cb21f2d2def9342c6a44ca564
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-02-01 18:11:58 -0300

    feat(useSignIn): implement sign-in logic

--- src/hooks/useSignIn.tsx
+++ src/hooks/useSignIn.tsx
@@ -1,31 +1,36 @@
+import { useApollo } from "@/apollo/useApollo"
 import { ValuesProps } from "../../interfaces"
 import { useRouter } from "next/router"
-import { toast } from "react-toastify"
-// import { setCookie } from "cookies-next"
+import { LOGIN_USER } from "@/graphql/mutations"
+import { useEffect } from "react"
+import { setCookie } from "cookies-next"
 
 export const useSignIn = () => {
   const router = useRouter()
-  // onSuccess: res => {
-  //   const tokens = res.data.body.tokens
-  //   localStorage.setItem("accessToken", tokens.access.token)
-  //   localStorage.setItem("refreshToken", tokens.refresh.token)
-  //   const accessToken = tokens.access.token
-  //   console.log(accessToken)
-  //   setCookie("auth", tokens.access.token)
-  //   router.push("/")
-  //   toast.success("You have successfully logged in")
-  // },
-  // onError: (error: any) => {
-  //   if (error.request.status === 404) return toast.error("This account doesn't exist")
-  //   else return toast.error("An error has occurred, please try again later")
-  // }
+  const {
+    lazyMethod: loginUser,
+    error,
+    loading,
+    data
+  } = useApollo({
+    gqlType: LOGIN_USER
+  })
+
+  useEffect(() => {
+    if (!error && !loading && data) {
+      setCookie("accessToken", data.loginUser.access)
+      setCookie("refreshToken", data.loginUser.refresh)
+      router.push("/")
+    }
+  }, [loading])
 
   const handleSubmit = async (values: ValuesProps) => {
     const user = {
       email: values.email,
       password: values.password
     }
-    // handle submit
+    console.log(user)
+    loginUser({ variables: user })
   }
 
   return { handleSubmit }
