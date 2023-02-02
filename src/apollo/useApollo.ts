91b88c9172597dfaa7ecf9a74375689a4f7a86aa
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-02-02 17:30:48 -0300

    feat(useApollo): add ask for refresh token if is expired

--- src/apollo/useApollo.ts
+++ src/apollo/useApollo.ts
@@ -1,5 +1,7 @@
+import { useRefreshToken } from "@/hooks/useRefreshToken"
 import { NotificationFailure, NotificationSuccess } from "@/utils/Notifications"
 import { useLazyQuery, useMutation } from "@apollo/client"
+import { getCookie } from "cookies-next"
 
 export const useApollo = ({
   gqlType,
@@ -10,6 +12,7 @@ export const useApollo = ({
   setState?: any
   successMessage?: string
 }) => {
+  const { refreshCurrentToken } = useRefreshToken()
   const gqlMethod =
     gqlType.definitions[0].operation === "mutation" ? (useMutation as any) : (useLazyQuery as any)
 
@@ -20,7 +23,11 @@ export const useApollo = ({
     },
     onError: (error: any) => {
       console.error(error)
-      return NotificationFailure("An error has occurred while fetching data")
+      if (error.message === "Unauthorized") {
+        const refreshToken = getCookie("refreshToken")
+        if (refreshToken) refreshCurrentToken()
+      }
+      return NotificationFailure("An error has occurred while fetching data. Try again later.")
     }
   })
 
