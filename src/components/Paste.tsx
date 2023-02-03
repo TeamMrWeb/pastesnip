cf0635eec48a709cc6e3e5da667fcb48b4782b0c
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-02-03 14:02:53 -0300

    feat(Paste): add styles

--- src/components/Paste.tsx
+++ src/components/Paste.tsx
@@ -1,6 +1,16 @@
 import WorldIcon from "../../public/world-icon.svg"
 import Link from "next/link"
 
+interface RangesProps {
+  years: number
+  months: number
+  weeks: number
+  days: number
+  hours: number
+  minutes: number
+  seconds: number
+}
+
 export default function Paste({
   title,
   link,
@@ -12,12 +22,43 @@ export default function Paste({
   createdAt: string
   syntaxHighlight: string
 }) {
+  const formatCreatedAtDate = (createdAt: string) => {
+    const date = new Date(Number(createdAt))
+    const formatter = new Intl.RelativeTimeFormat("en")
+    const ranges: RangesProps = {
+      years: 3600 * 24 * 365,
+      months: 3600 * 24 * 30,
+      weeks: 3600 * 24 * 7,
+      days: 3600 * 24,
+      hours: 3600,
+      minutes: 60,
+      seconds: 1
+    }
+    const secondsElapsed = (date.getTime() - Date.now()) / 1000
+    for (let key in ranges) {
+      if (ranges[key as keyof RangesProps] < Math.abs(secondsElapsed)) {
+        const delta = secondsElapsed / ranges[key as keyof RangesProps]
+        return formatter.format(Math.round(delta), key as keyof RangesProps)
+      }
+    }
+  }
+
   return (
-    <li className="flex">
-      <WorldIcon className="w-5" />
-      <Link href={`paste/${link}`}>{title}</Link>
-      <p>{createdAt}</p>
-      <p>{syntaxHighlight}</p>
+    <li
+      className="w-full grid grid-cols-4 border-t border-green-3 p-1 px-0 box-border text last:border-b"
+      style={{ fontSize: "clamp(.8rem, 1vw, 1rem)" }}
+    >
+      <div className="flex items-center justify-center gap-2 w-max col-span-2">
+        <WorldIcon className="w-5" />
+        <Link
+          href={`paste/${link}`}
+          className="text-[#7abefe] leading-4 h-5 justify-self-start hover:text-[#9fcffc]"
+        >
+          {title}
+        </Link>
+      </div>
+      <p>{formatCreatedAtDate(createdAt)}</p>
+      <p className="justify-self-end">{syntaxHighlight}</p>
     </li>
   )
 }
