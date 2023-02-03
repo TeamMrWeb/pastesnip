ea5481022c92f998aa88c7c270ebbaf1b3145242
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-02-03 14:08:30 -0300

    feat(userTab): add signOut

--- src/components/UserTab.tsx
+++ src/components/UserTab.tsx
@@ -1,13 +1,23 @@
-import Link from "next/link"
+import { useSignOut } from "@/hooks/useSignOut"
 import UserIcon from "../../public/user-icon.svg"
+import { useRef, useState } from "react"
+import { useHandleClick } from "@/hooks/useHandleClick"
 
 export default function UserTab() {
+  const { signOut } = useSignOut()
+  const [showDropdown, setShowDropdown] = useState(false)
+  const dropdownRef = useRef<HTMLUListElement>(null)
+  const buttonRef = useRef<HTMLLabelElement>(null)
+  useHandleClick(showDropdown, dropdownRef.current!, buttonRef.current!, setShowDropdown)
+
   return (
     <>
       <div className="dropdown !bg-green-1 !hover:bg-green-8">
         <label
           tabIndex={0}
           className="btn m-1 flex items-center gap-4 bg-green-1 text-green-8 hover:!bg-green-2"
+          ref={buttonRef}
+          onClick={() => setShowDropdown(!showDropdown)}
         >
           Finisterix
           <UserIcon className="w-5 fill-green-4" />
@@ -15,9 +25,10 @@ export default function UserTab() {
         <ul
           tabIndex={0}
           className="dropdown-content menu p-2 shadow rounded-box w-52 bg-green-1 border border-green-2"
+          ref={dropdownRef}
         >
           <li>
-            <Link href="/">Log out</Link>
+            <button onClick={signOut}>Log out</button>
           </li>
         </ul>
       </div>
