e2af2697786424db083da30245d030ac733342a1
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-02-05 16:04:23 -0300

    feat(PageLayout): add LoadingBar

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -3,8 +3,11 @@ import Header from "./Header"
 import logo from "../../public/logo.png"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
+import LoadingBar from "react-top-loading-bar"
+import { useTopLoaderContext } from "@/contexts/TopLoaderContext"
 
 export default function PageLayout({ children }: { children: React.ReactNode }) {
+  const { progress, setProgress } = useTopLoaderContext()
   return (
     <div className="w-full h-full min-h-screen bg-green-1">
       <Head>
@@ -13,6 +16,12 @@ export default function PageLayout({ children }: { children: React.ReactNode }) 
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href={logo.src} />
       </Head>
+      <LoadingBar
+        color="#40916C"
+        height={3}
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
       <Header />
       <ToastContainer
         position="top-center"
@@ -26,7 +35,7 @@ export default function PageLayout({ children }: { children: React.ReactNode }) 
         pauseOnHover
       />
       <main className="flex place-content-center box-border">
-        <div className="w-full max-w-maximum h-full p-5 pt-20 box-border bg-green-2">
+        <div className="w-full max-w-maximum h-full p-5 pt-13 box-border bg-green-2">
           {children}
         </div>
       </main>
