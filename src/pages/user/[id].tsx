35834597cf7426f1842eaefbc9d9d66cd356f21e
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-02-05 15:33:05 -0300

    feat: add user page

---
+++ src/pages/user/[id].tsx
@@ -0,0 +1,48 @@
+import { serverSideApolloFetching } from "@/apollo/serverSideApolloFetching"
+import Paste from "@/components/Paste"
+import PasteUserTab from "@/components/PasteUserTab"
+import { PasteProps } from "interfaces"
+
+export default function User({ pastes }: { pastes: PasteProps[] }) {
+  return (
+    <>
+      <PasteUserTab pasteTitle="Finisterix" createdAt="23/93" />
+      <ul className="flex flex-col w-full mt-5">
+        {pastes &&
+          pastes.map((paste: PasteProps) => (
+            <Paste
+              key={paste.id}
+              title={paste.title}
+              link={paste.id}
+              createdAt={paste.createdAt}
+              syntaxHighlight={paste.syntax_highlight}
+            />
+          ))}
+      </ul>
+    </>
+  )
+}
+
+// export async function getServerSideProps({ req, res, context }: { req: any; res: any; context: any }) {
+// const accessToken = req.cookies["accessToken"]
+//   if (!accessToken) {
+//     return {
+//       redirect: {
+//         permanent: false,
+//         destination: "/signin"
+//       }
+//     }
+//   }
+//   const data = await serverSideApolloFetching({
+//     fetch: "query",
+//     req,
+//     res,
+//     schema: PASTES
+//   })
+//   const pastes = data.data.pastes
+//   return {
+//     props: {
+//       pastes
+//     }
+//   }
+// }
