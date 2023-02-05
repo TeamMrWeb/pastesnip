01ac9925cece9728ac591c260c38be8348928a9a
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-02-05 13:42:26 -0300

    feat(useSignIn): add send email verification after log in

--- src/hooks/useSignIn.tsx
+++ src/hooks/useSignIn.tsx
@@ -1,34 +1,41 @@
 import { useApollo } from "@/apollo/useApollo"
 import { ValuesProps } from "../../interfaces"
-import { useRouter } from "next/router"
-import { LOGIN_USER } from "@/graphql/mutations"
-import { useEffect } from "react"
+import { LOGIN_USER, SEND_VERIFICATION_EMAIL } from "@/graphql/mutations"
 import { cookies } from "@/utils/Cookies"
+import { LOGGED_USER } from "@/graphql/queries"
+import { useRouter } from "next/router"
 
 export const useSignIn = () => {
   const router = useRouter()
-  const {
-    lazyMethod: loginUser,
-    error,
-    loading,
-    data
-  } = useApollo({
+  const { lazyMethod: loginUser, loginError } = useApollo({
     gqlType: LOGIN_USER
   })
-
-  useEffect(() => {
-    if (!error && !loading && data) {
-      cookies({ accessToken: data.loginUser.access, refreshToken: data.loginUser.refresh })
-      router.push("/")
-    }
-  }, [loading])
+  const {
+    lazyMethod: getLoggedUser,
+    userError,
+    userData
+  } = useApollo({
+    gqlType: LOGGED_USER
+  })
+  const { lazyMethod: sendVerificationEmail } = useApollo({
+    gqlType: SEND_VERIFICATION_EMAIL,
+    successMessage:
+      "An email has been sent to your email address. Please verify your account to unlock all the features."
+  })
 
   const handleSubmit = async (values: ValuesProps) => {
     const user = {
       email: values.email,
       password: values.password
     }
-    loginUser({ variables: user })
+    const { data: loginData } = await loginUser({ variables: user })
+    cookies({
+      accessToken: loginData.loginUser.access,
+      refreshToken: loginData.loginUser.refresh
+    })
+    if (!loginError && loginData) await getLoggedUser()
+    if (!userError && !userData?.verified) await sendVerificationEmail()
+    router.push("/")
   }
 
   return { handleSubmit }
