863598cf54a468745de773ef82da19e4e18c52ad
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-02-05 16:03:33 -0300

    feat(useApollo): add set progress on every request

--- src/apollo/useApollo.ts
+++ src/apollo/useApollo.ts
@@ -1,3 +1,4 @@
+import { useTopLoaderContext } from "@/contexts/TopLoaderContext"
 import { useRefreshToken } from "@/hooks/useRefreshToken"
 import { NotificationFailure, NotificationSuccess } from "@/utils/Notifications"
 import { useLazyQuery, useMutation } from "@apollo/client"
@@ -13,16 +14,20 @@ export const useApollo = ({
   successMessage?: string
 }) => {
   const { refreshCurrentToken } = useRefreshToken()
+  const { progress, setProgress } = useTopLoaderContext()
   const gqlMethod =
     gqlType.definitions[0].operation === "mutation" ? (useMutation as any) : (useLazyQuery as any)
+  setProgress(0)
 
   const [lazyMethod, { ...props }] = gqlMethod(gqlType, {
     onCompleted: (data: any) => {
       setState && setState(data)
       successMessage && NotificationSuccess(successMessage)
+      setProgress(progress + 100)
     },
     onError: (error: any) => {
       console.error(error)
+      setProgress(progress + 100)
       if (error.message === "Unauthorized") {
         const refreshToken = getCookie("refreshToken")
         if (refreshToken) refreshCurrentToken()
