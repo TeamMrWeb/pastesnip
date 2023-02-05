2c7bab1b6e8c162723f3db8428b2f14f6ca68457
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-02-05 15:36:57 -0300

    feat(UserTab): add "my profile" link

--- src/components/UserTab.tsx
+++ src/components/UserTab.tsx
@@ -3,6 +3,7 @@ import UserIcon from "../../public/user-icon.svg"
 import { useRef, useState } from "react"
 import { useHandleClick } from "@/hooks/useHandleClick"
 import { useLoggedUserContext } from "@/contexts/LoggedUserContext"
+import Link from "next/link"
 
 export default function UserTab() {
   const { signOut } = useSignOut()
@@ -30,6 +31,9 @@ export default function UserTab() {
           className="dropdown-content menu p-2 shadow rounded-box w-52 bg-green-1 border border-green-2"
           ref={dropdownRef}
         >
+          <li>
+            <Link href={`/user/${loggedUser.id}`}>Profile</Link>
+          </li>
           <li>
             <button onClick={signOut}>Log out</button>
           </li>
