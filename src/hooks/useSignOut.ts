901bb75dc487d588157dcca7913c465fec4fa99b
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-02-06 16:06:35 -0300

    feat(useSignOut): add both apollo and context cache clear

--- src/hooks/useSignOut.ts
+++ src/hooks/useSignOut.ts
@@ -1,12 +1,22 @@
+import { useLoggedUserContext } from "@/contexts/LoggedUserContext"
 import { deleteCookie } from "cookies-next"
 import { useRouter } from "next/router"
+import { initializeApollo } from "@/apollo"
 
 export const useSignOut = () => {
   const route = useRouter()
+  const { setLoggedUser } = useLoggedUserContext()
+  const apolloClient = initializeApollo()
+
   const signOut = () => {
+    deleteCookie("auth")
+    deleteCookie("refresh")
     deleteCookie("accessToken")
     deleteCookie("refreshToken")
+    apolloClient.clearStore()
+    setLoggedUser({})
     route.push("/signin")
   }
+
   return { signOut }
 }
