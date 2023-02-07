7cb9dfea98f1c212f00752d3f25722eeadc5eede
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-02-07 13:43:16 -0300

    feat(paste): add logged user verified verification

--- src/pages/paste/[id].tsx
+++ src/pages/paste/[id].tsx
@@ -1,57 +1,70 @@
 import { serverSideApolloFetching } from "@/apollo/serverSideApolloFetching"
 import PasteUserTab from "@/components/PasteUserTab"
 import Select from "@/components/Select"
+import { LOGGED_USER } from "@/graphql/queries"
 import { useState } from "react"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism"
 
-export default function Paste() {
+export default function Paste({ verified }: { verified: boolean }) {
   const [languageHighlight, setLanguageHighlight] = useState("jsx")
 
   return (
     <div className="flex flex-col gap-5">
-      <header className="flex justify-between flex-wrap gap-y-3">
-        <PasteUserTab pasteTitle="cosa" subtitle="finsiterix" createdAt="23/93" />
-        <div className="w-full max-w-[340px]">
-          <Select
-            name="Syntax Highlight"
-            firstValue="Select Syntax"
-            options={["jsx", "javascript", "php"]}
-            setValue={setLanguageHighlight}
-          />
-        </div>
-      </header>
-      <section>
-        <SyntaxHighlighter
-          language={languageHighlight}
-          style={atomDark}
-          className={`w-full min-h-[500px] !m-0 transition-all duration-1000`}
-          showLineNumbers={true}
-        >
-          hoalsdoaslods
-        </SyntaxHighlighter>
-      </section>
+      {!verified ? (
+        <h1 className="text-white text-xl">You must verify your account to see this content</h1>
+      ) : (
+        <>
+          <header className="flex justify-between flex-wrap gap-y-3">
+            <PasteUserTab pasteTitle="cosa" subtitle="finsiterix" createdAt="23/93" />
+            <div className="w-full max-w-[340px]">
+              <Select
+                name="Syntax Highlight"
+                firstValue="Select Syntax"
+                options={["jsx", "javascript", "php"]}
+                setValue={setLanguageHighlight}
+              />
+            </div>
+          </header>
+          <section>
+            <SyntaxHighlighter
+              language={languageHighlight}
+              style={atomDark}
+              className={`w-full min-h-[500px] !m-0 transition-all duration-1000`}
+              showLineNumbers={true}
+            >
+              hoalsdoaslods
+            </SyntaxHighlighter>
+          </section>
+        </>
+      )}
     </div>
   )
 }
 
-// export async function getServerSideProps({
-//   req,
-//   res,
-//   context
-// }: {
-//   req: any
-//   res: any
-//   context: any
-// }) {
-//   // const { id } = context.query
-//   // const data = await serverSideApolloFetching({
-//   //   fetch: "query",
-//   //   req,
-//   //   res,
-//   //   schema: LOGGED_USER
-//   // })
-//   // return {
-//   //   props: { data }
-//   // }
-// }
+export async function getServerSideProps({ req, res }: { req: any; res: any }) {
+  const data = await serverSideApolloFetching({
+    fetch: "query",
+    req,
+    res,
+    schema: LOGGED_USER
+  })
+  const loggedUser = data.data.me
+  const accessToken = req.cookies["accessToken"]
+  if (!accessToken) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/signin"
+      }
+    }
+  }
+  // todo: get paste by id
+
+  return {
+    props: {
+      // paste
+      verified: loggedUser.verified
+    }
+  }
+}
