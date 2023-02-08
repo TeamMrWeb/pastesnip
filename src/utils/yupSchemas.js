cfd24da43444904b7bd7217e3caf6d0810643c3b
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-02-08 13:51:22 -0300

    fix(pasteSchema): fix title regex

--- src/utils/yupSchemas.js
+++ src/utils/yupSchemas.js
@@ -31,7 +31,7 @@ export const signInSchema = {
 export const pasteSchema = {
   title: Yup.string()
     .max(20, "Must contain 20 characters or less")
-    .matches(/^[a-zA-Z]*$/, "Must contain only letters")
+    .matches(/^[A-Za-z0-9\s\-_,\.;:()]+$/, "Must contain only letters")
     .required("Required"),
   syntaxHighlight: Yup.string().required("Required"),
   tags: Yup.string()
